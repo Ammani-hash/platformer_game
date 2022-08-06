fe909e5a1601269712812aa542d44211e2ceabfd
Ammanuel <wondwossenammanuel@gmail.com>
2022-08-05 19:03:35 -0500

    Uploading python game -Created a new repository for Multilevel game. -Learned how to upload files into a directory via command line.

---
+++ platformer_game/New map.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="New Map" tilewidth="128" tileheight="128" tilecount="25" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/boxCrate_double.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/coin_01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/femaleAdventurer_idle.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassLeft.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassMid.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassRight.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/mushroomRed.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/wormGreen.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/wormGreen_move.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/flagGreen1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/frog.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grass_sprout.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/slimeBlock.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/bridgeB.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassCenter.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassHalf_left.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassHalf_mid.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassHalf_right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/signRight.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/water.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/waterTop_high.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/waterTop_low.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/spikes.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassHill_left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../Desktop/School/Programming/arcade projects for students/arcade images/More Arcade graphics/grassHill_right.png"/>
+ </tile>
+</tileset>
